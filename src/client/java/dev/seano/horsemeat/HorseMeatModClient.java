164e78562ba1c9e1d5f8bc7de05f8da9f2705290
Sean O'Connor <so@seano.dev>
2023-08-21 09:25:49 -0600

    Initial commit

---
+++ src/client/java/dev/seano/horsemeat/HorseMeatModClient.java
@@ -0,0 +1,10 @@
+package dev.seano.horsemeat;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HorseMeatModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
