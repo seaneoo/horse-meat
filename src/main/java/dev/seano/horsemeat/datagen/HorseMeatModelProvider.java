3543164ef9795f89eb14e9e4a25facac14a46e24
Sean O'Connor <so@seano.dev>
2023-09-07 16:51:29 -0600

    Run data generation
    Update textures for raw and cooked horse and llama meat

--- src/main/java/dev/seano/horsemeat/datagen/HorseMeatModelProvider.java
+++ src/main/java/dev/seano/horsemeat/datagen/HorseMeatModelProvider.java
@@ -8,6 +8,7 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
 import net.minecraft.data.client.Models;
+import net.minecraft.item.Items;
 
 public class HorseMeatModelProvider extends FabricModelProvider {
 	public HorseMeatModelProvider(FabricDataOutput output) {
@@ -21,10 +22,10 @@ public class HorseMeatModelProvider extends FabricModelProvider {
 
 	@Override
 	public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-		itemModelGenerator.register(HorseMeatItems.RAW_HORSE_MEAT, Models.GENERATED);
-		itemModelGenerator.register(HorseMeatItems.COOKED_HORSE_MEAT, Models.GENERATED);
-		itemModelGenerator.register(HorseMeatItems.RAW_LLAMA_MEAT, Models.GENERATED);
-		itemModelGenerator.register(HorseMeatItems.COOKED_LLAMA_MEAT, Models.GENERATED);
+		itemModelGenerator.register(HorseMeatItems.RAW_HORSE_MEAT, Items.BEEF, Models.GENERATED);
+		itemModelGenerator.register(HorseMeatItems.COOKED_HORSE_MEAT, Items.COOKED_BEEF, Models.GENERATED);
+		itemModelGenerator.register(HorseMeatItems.RAW_LLAMA_MEAT, Items.BEEF, Models.GENERATED);
+		itemModelGenerator.register(HorseMeatItems.COOKED_LLAMA_MEAT, Items.COOKED_BEEF, Models.GENERATED);
 		itemModelGenerator.register(HorseMeatItems.HORSE_BURGER, Models.GENERATED);
 	}
 }
