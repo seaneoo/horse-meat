ad07d9c6fc2574bb407601226bed22f1f7035247
Sean O'Connor <so@seano.dev>
2023-09-18 09:00:22 -0600

    Migrate to RecipeExporter

--- src/main/java/dev/seano/horsemeat/datagen/HorseMeatRecipeProvider.java
+++ src/main/java/dev/seano/horsemeat/datagen/HorseMeatRecipeProvider.java
@@ -2,16 +2,14 @@ package dev.seano.horsemeat.datagen;
 
 import dev.seano.horsemeat.HorseMeatItems;
 
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-
-import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.recipe.book.RecipeCategory;
 
-import java.util.function.Consumer;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 
 public class HorseMeatRecipeProvider extends FabricRecipeProvider {
 	public HorseMeatRecipeProvider(FabricDataOutput output) {
@@ -19,12 +17,12 @@ public class HorseMeatRecipeProvider extends FabricRecipeProvider {
 	}
 
 	@Override
-	public void generate(Consumer<RecipeJsonProvider> exporter) {
+	public void generate(RecipeExporter exporter) {
 		generateSmeltingRecipes(exporter);
 		generateCraftingRecipes(exporter);
 	}
 
-	private void generateSmeltingRecipes(Consumer<RecipeJsonProvider> exporter) {
+	private void generateSmeltingRecipes(RecipeExporter exporter) {
 		final float EXPERIENCE = 0.35f; // How much experience is gained
 		final int COOKING_TIME = 200; // Num. of ticks to "cook" (200 ticks is 10 seconds)
 
@@ -45,7 +43,7 @@ public class HorseMeatRecipeProvider extends FabricRecipeProvider {
 				HorseMeatItems.RAW_LLAMA_MEAT, HorseMeatItems.COOKED_LLAMA_MEAT, EXPERIENCE);
 	}
 
-	private void generateCraftingRecipes(Consumer<RecipeJsonProvider> exporter) {
+	private void generateCraftingRecipes(RecipeExporter exporter) {
 		ShapelessRecipeJsonBuilder.create(RecipeCategory.FOOD, HorseMeatItems.HORSE_BURGER)
 				.input(HorseMeatItems.COOKED_HORSE_MEAT).input(Items.BREAD)
 				.criterion(hasItem(HorseMeatItems.COOKED_HORSE_MEAT),
